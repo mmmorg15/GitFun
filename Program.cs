ef009f069ff9bc5a72c3ed025cbfd435377be71b
mmmorg15 <mmmorg15@byu.edu>
2026-01-27 18:37:55 -0700

    writeline

--- Program.cs
+++ Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+
+Console.Writeline("This is a sample C# 14.0 program running on .NET 10.0.");
